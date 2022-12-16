f163ccdc33620ae24722b23d8f8f546357c603a6
avs-abhishek123 <abhishek.avs123@gmail.com>
2022-12-16 22:16:47 +0530

    video capture with fps module updated

--- test-webcam-video.py
+++ test-webcam-video.py
@@ -1,4 +1,4 @@
-from module.webcam_video_capture import webcam_video_capture_class as wcam
+from module.webcam_video_capture_with_FPS import webcam_video_capture_class as wcam
 
 wcam_object = wcam("Test")
 wcam_object.VideoCapture()
